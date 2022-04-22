c70b944b78812c79e7adff993fcd26b031e1385e
Anish <GA352491@gmail.com>
2022-04-22 17:10:17 +0530

    Update settings.py

--- fulldjango/settings.py
+++ fulldjango/settings.py
@@ -152,3 +152,38 @@ CORS_ALLOWED_ORIGINS = [
     '*'
 ]
 CORS_ORIGIN_ALLOW_ALL = True
+ORS_ALLOW_CREDENTIALS = True
+ACCESS_CONTROL_ALLOW_HEADERS = True
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:4200',
+    'localhost:8000',
+    '127.0.0.1:8000',
+
+)
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
+CSRF_TRUSTED_ORIGINS = (
+    '127.0.0.1:9200',
+    '*'
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
